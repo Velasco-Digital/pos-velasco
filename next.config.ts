c36f09f05d917a1ecf57a820d0c9ee38a36511cd
Velasco-Digital <vladirblack@gmail.com>
2026-04-09 20:19:53 -0600

    final: limpieza de config y llaves de supabase

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* Configuración para saltar errores y desplegar rápido */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
-
 export default nextConfig;
