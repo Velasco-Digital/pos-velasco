12be50b6a2a53c5c25165f22ea87a2821f50fdd9
Velasco-Digital <vladirblack@gmail.com>
2026-04-13 02:25:59 -0600

    SOLUCIÓN: Carpeta lib corregida y archivos en su lugar

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,5 +4,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://koatzrklsdukvaafqrkk.supabase.co';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvYXR6cmtsc2R1a3ZhYWZxcmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NjA4MzcsImV4cCI6MjA5MTQzNjgzN30.QeBzXCyO4ERss78JKba4TM-uDN7y6ghCYvcO2PWXX8I';
 
-// Solo creamos el cliente si las llaves existen de verdad
+// Solo creamos el cliente si las llaves existen de v
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
